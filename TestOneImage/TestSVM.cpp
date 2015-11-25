e8497ab430b7106a482e7900ea8b288ed024bc11
korujzade <oruczade.kamil@gmail.com>
2015-11-25 00:04:55 +0000

    real time application first try - done

--- TestOneImage/TestSVM.cpp
+++ TestOneImage/TestSVM.cpp
@@ -17,31 +17,66 @@ using namespace std;
 clock_t t1, t2;
 
 
-int testRecord(char filen[100]);
+int testRecord(Mat img);
 // load trained svm xml
 Ptr<SVM> svm = StatModel::load<SVM>("/home/korujzade/Desktop/DriveSafe/backend/TrainSVM/trainedSVM.xml");
+Ptr<SVM> svm2 = StatModel::load<SVM>("/home/korujzade/Desktop/DriveSafe/backend/TrainSVM/trainedSVM2.xml");
+
 
 int main(int argc, char *argv[])
 {
   t1 = clock();
+	VideoCapture cap(0);
+	if(!cap.isOpened())
+		return -1;
 
-  int result = testRecord(argv[1]);
+	Mat edges;
+	namedWindow("Camera", CV_WINDOW_NORMAL);
+	int count = 0;
+	for(;;)
+	{
+		count++;
+		Mat frame;
+		bool success = cap.read(frame);
+		if(!success)
+		{
+			cout << "Cannot read frame" << endl;
+			break;
+		}
+		int result = testRecord(frame);
 
-  cout << result << endl;
+		// if (result == 1)
+		// {
+		// 	cout << "Alert Alert Alert!!!" << endl;
+		// 	imwrite("frame.jpg", frame);
+		// 	break;
+		// }
+		if (result == 1)
+		{
+			cout << "Alert Alert Alert!!! "<< endl;
+		}	
+			
+		//cout << result << endl;	
+		imshow("Camera", frame);
+		if(waitKey(30) >= 0) break;
+	}	
+
+//  int result = testRecord(argv[1]);
+
+  
  
   t2 = clock();
   // float diff ((float)t2 - (float)t1);
   // cout << diff << endl;
 }
 
-int testRecord(char filen[100])
+int testRecord(Mat img)
 {
-	Mat img = imread(filen);
 	Mat grayImg;
 
 	resize(img, img, Size(128,128));
 	cvtColor(img, grayImg, CV_RGB2GRAY);
-	HOGDescriptor hog(Size(128,128), Size(8,8), Size(4,4), Size(4,4), 9);
+	HOGDescriptor hog(Size(64,64), Size(16,16), Size(8,8), Size(8,8), 9);
 	vector <float> descriptors;
 	vector <Point> locations;
 	hog.compute(grayImg, descriptors, Size(0,0), Size(0,0), locations);
@@ -56,6 +91,10 @@ int testRecord(char filen[100])
 	copy(tmp.begin<float>(), tmp.end<float>(), newsampleMat.begin<float>());
 
 	int res = svm->predict(newsampleMat);
+	int res2 = svm2->predict(newsampleMat);
+
+	if (res == 1 || res2 ==1 )
+		res = 1;
 
 	return res;
 }
